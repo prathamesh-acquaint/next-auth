1e89b962904fe9ea95c309fa7e1a6a1f8466f918
Prathamesh <prathamesh.trivedi@acquaintsoft.com>
2024-05-15 19:50:25 +0530

    Register User Done

--- actions/register.ts
+++ actions/register.ts
@@ -1,11 +1,29 @@
 "use server";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
+import bcrypt from "bcryptjs";
 
 export const Register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
 
   if (!validatedFields.success) return { error: "Invalid Fields!" };
 
-  return { success: "Email Sent!" };
+  const { email, name, password } = validatedFields.data;
+
+  const existingUser = await db.user.findUnique({ where: { email } });
+
+  if (existingUser) return { error: "User already exists!" };
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  return { success: "User Created Successfully" };
 };
